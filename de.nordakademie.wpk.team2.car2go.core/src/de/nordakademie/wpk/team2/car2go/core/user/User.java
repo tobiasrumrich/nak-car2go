dfbc7f2faacccdb075638dc7d20a5ce02c47f318
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@bff13046-3d4e-0f97-a4d0-0daf6970e2a8>
2010-12-01 14:19:00 +0000

    Added user package

---
+++ de.nordakademie.wpk.team2.car2go.core/src/de/nordakademie/wpk/team2/car2go/core/user/User.java
@@ -0,0 +1,11 @@
+package de.nordakademie.wpk.team2.car2go.core.user;
+
+
+
+import de.nordakademie.wpk.team2.car2go.core.CarStorage;
+
+public class User {
+	private String username;
+	private String fullname;
+	private CarStorage bookmarked_cars;
+}

b29a04f836a1f43bdb80928cea426adfe7fc2de9
alexander.westen@gmail.com <alexander.westen@gmail.com@bff13046-3d4e-0f97-a4d0-0daf6970e2a8>
2010-12-01 16:56:35 +0000

    01.12.2010

--- de.nordakademie.wpk.team2.car2go.product/src/de/nordakademie/wpk/team2/car2go/product/Perspective.java
+++ de.nordakademie.wpk.team2.car2go.product/src/de/nordakademie/wpk/team2/car2go/product/Perspective.java
@@ -3,12 +3,19 @@ package de.nordakademie.wpk.team2.car2go.product;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+/*
+ * This perspective includes the Car2goView.
+ * 
+ * @Autor: Alexander Westen, Matthias Lüders
+ */
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
-//		layout.setEditorAreaVisible(true);
-//		layout.addStandaloneView("de.nordakademie.wpk.todolist.ui.TodolistView", true, IPageLayout.LEFT, 0.5f, layout.getEditorArea());
-//		layout.setFixed(true);
+		layout.setEditorAreaVisible(true);
+		layout.addStandaloneView(
+				"de.nordakademie.wpk.team2.car2go.ui.views.Car2goView", true,
+				IPageLayout.LEFT, 0.65f, layout.getEditorArea());
+		layout.setFixed(true);
 	}
 
 }

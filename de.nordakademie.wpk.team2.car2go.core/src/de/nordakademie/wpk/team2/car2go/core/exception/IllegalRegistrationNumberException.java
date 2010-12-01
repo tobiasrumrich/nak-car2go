a2ed9303c7f8c8914313f5986ad02801dbfc5b02
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@bff13046-3d4e-0f97-a4d0-0daf6970e2a8>
2010-12-01 14:18:42 +0000

    Added exception package

---
+++ de.nordakademie.wpk.team2.car2go.core/src/de/nordakademie/wpk/team2/car2go/core/exception/IllegalRegistrationNumberException.java
@@ -0,0 +1,24 @@
+package de.nordakademie.wpk.team2.car2go.core.exception;
+
+public class IllegalRegistrationNumberException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getLocalizedMessage() {
+		return "Das Kennzeichen ist ungültig oder eine mehrfache Nutzung des Kennzeichens wurde durch eine ICarStorage festgestellt.";
+		// return super.getLocalizedMessage();
+	}
+
+	@Override
+	public String getMessage() {
+		return "The registration number of this car is either not valid or a multiple usage of a registration number has been detected by an ICarStorage.";
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
---
+++ de.nordakademie.wpk.team2.car2go.core/src/de/nordakademie/wpk/team2/car2go/core/exception/RegistrationNumberNotFoundException.java
@@ -0,0 +1,23 @@
+package de.nordakademie.wpk.team2.car2go.core.exception;
+
+public class RegistrationNumberNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getLocalizedMessage() {
+		return "Ein Fahrzeug mit diesem Kennzeichen konnte nicht gefunden werden.";
+		// return super.getLocalizedMessage();
+	}
+
+	@Override
+	public String getMessage() {
+		return "A car with this registration number could not be found.";
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}

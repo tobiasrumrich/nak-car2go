6ad49280293ffbc2e948a3269e4ca19da9267010
alexander.westen@gmail.com <alexander.westen@gmail.com@bff13046-3d4e-0f97-a4d0-0daf6970e2a8>
2010-12-01 14:36:46 +0000

    Kleiner zwischenstand

--- de.nordakademie.wpk.team2.car2go.ui/src/de/nordakademie/wpk/team2/car2go/ui/editor/CarEditor.java
+++ de.nordakademie.wpk.team2.car2go.ui/src/de/nordakademie/wpk/team2/car2go/ui/editor/CarEditor.java
@@ -12,6 +12,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
+import com.swtdesigner.ResourceManager;
 
 public class CarEditor extends EditorPart {
 	private Text txtRegistrationNumber;
@@ -20,7 +21,7 @@ public class CarEditor extends EditorPart {
 	private Text txtInteriorState;
 	private Text txtGeoPoint;
 	private Text txtLocation;
-	private Text text_8;
+	private Text txtComment;
 
 	public CarEditor() {
 		// TODO Auto-generated constructor stub
@@ -66,58 +67,68 @@ public class CarEditor extends EditorPart {
 		lblRegistrationNumber.setText("Kennzeichen");
 		
 		txtRegistrationNumber = new Text(parent, SWT.BORDER);
-		txtRegistrationNumber.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		GridData gd_txtRegistrationNumber = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_txtRegistrationNumber.minimumWidth = 200;
+		txtRegistrationNumber.setLayoutData(gd_txtRegistrationNumber);
 		
 		Label lblFuelState = new Label(parent, SWT.NONE);
 		lblFuelState.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblFuelState.setText("Tankstand");
 		
 		txtFuelState = new Text(parent, SWT.BORDER);
-		txtFuelState.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		GridData gd_txtFuelState = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_txtFuelState.minimumWidth = 200;
+		txtFuelState.setLayoutData(gd_txtFuelState);
 		
 		Label lblExteriorState = new Label(parent, SWT.NONE);
 		lblExteriorState.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblExteriorState.setText("Au\u00DFenzustand");
 		
 		txtExteriorState = new Text(parent, SWT.BORDER);
-		txtExteriorState.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		GridData gd_txtExteriorState = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_txtExteriorState.minimumWidth = 200;
+		txtExteriorState.setLayoutData(gd_txtExteriorState);
 		
 		Label lblInteriorState = new Label(parent, SWT.NONE);
 		lblInteriorState.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblInteriorState.setText("Innenzustand");
 		
 		txtInteriorState = new Text(parent, SWT.BORDER);
-		txtInteriorState.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		GridData gd_txtInteriorState = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_txtInteriorState.minimumWidth = 200;
+		txtInteriorState.setLayoutData(gd_txtInteriorState);
 		
 		Label lblGeoPoint = new Label(parent, SWT.NONE);
 		lblGeoPoint.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblGeoPoint.setText("Aktuelle Geokoordinate");
 		
 		txtGeoPoint = new Text(parent, SWT.BORDER);
-		txtGeoPoint.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		GridData gd_txtGeoPoint = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_txtGeoPoint.minimumWidth = 200;
+		txtGeoPoint.setLayoutData(gd_txtGeoPoint);
 		
 		Label lblLocation = new Label(parent, SWT.NONE);
 		lblLocation.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblLocation.setText("Aktueller Standort");
 		
 		txtLocation = new Text(parent, SWT.BORDER);
-		txtLocation.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		GridData gd_txtLocation = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_txtLocation.minimumWidth = 200;
+		txtLocation.setLayoutData(gd_txtLocation);
 		
 		Label lblVacant = new Label(parent, SWT.NONE);
 		lblVacant.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblVacant.setText("Vakant");
+		
+		Label label = new Label(parent, SWT.NONE);
+		label.setImage(ResourceManager.getPluginImage("de.nordakademie.wpk.team2.car2go.ui", "resources/icons/no.png"));
+		
+		Label lblcomment = new Label(parent, SWT.NONE);
+		lblcomment.setText("Bemerkung");
 		new Label(parent, SWT.NONE);
 		
-		Label label_7 = new Label(parent, SWT.NONE);
-		label_7.setText("New Label");
-		new Label(parent, SWT.NONE);
-		
-		Label label_8 = new Label(parent, SWT.NONE);
-		label_8.setText("New Label");
-		new Label(parent, SWT.NONE);
-		
-		text_8 = new Text(parent, SWT.BORDER);
-		text_8.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		txtComment = new Text(parent, SWT.BORDER | SWT.MULTI);
+		txtComment.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
 		// TODO Auto-generated method stub
 
 	}
---
+++ de.nordakademie.wpk.team2.car2go.ui/src/de/nordakademie/wpk/team2/car2go/ui/handler/CarEditHandler.java
@@ -0,0 +1,19 @@
+package de.nordakademie.wpk.team2.car2go.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class CarEditHandler extends AbstractHandler {
+
+	/*
+	 * Diese Methode ruft den CarEditor für das im Tree selektierte Car auf.
+	 * @Author: Alexander Westen & Matthias Lüders
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

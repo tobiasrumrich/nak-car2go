da402be5728f406f7c8cf86c7e9ac9c884888f94
themanwithnoname237 <themanwithnoname237@bff13046-3d4e-0f97-a4d0-0daf6970e2a8>
2010-12-05 01:39:39 +0000

    - Fixed bugs

--- de.nordakademie.wpk.team2.car2go.core/src/de/nordakademie/wpk/team2/car2go/core/BookmarkStorage.java
+++ de.nordakademie.wpk.team2.car2go.core/src/de/nordakademie/wpk/team2/car2go/core/BookmarkStorage.java
@@ -7,18 +7,32 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 
 import de.nordakademie.wpk.team2.car2go.core.businessobjects.Car;
+import de.nordakademie.wpk.team2.car2go.core.exception.DuplicateBookmarkException;
+import de.nordakademie.wpk.team2.car2go.core.exception.IllegalRegistrationNumberException;
+import de.nordakademie.wpk.team2.car2go.core.exception.IllegalUsernameException;
 import de.nordakademie.wpk.team2.car2go.core.exception.RegistrationNumberNotFoundException;
+import de.nordakademie.wpk.team2.car2go.core.exception.UsernameNotFoundException;
 import de.nordakademie.wpk.team2.car2go.interfaces.IBookmarkStorage;
 import de.nordakademie.wpk.team2.car2go.interfaces.ICar;
+import de.nordakademie.wpk.team2.car2go.interfaces.ICarRegistrationNumberValidator;
 
 public class BookmarkStorage implements IBookmarkStorage {
 	private static final Logger logger = Logger.getLogger(BookmarkStorage.class);
 	private HashMap<String, HashSet<ICar>> bookmarks = new HashMap<String, HashSet<ICar>>();
+	private ICarRegistrationNumberValidator registrationNumberValidator = new GenericRegistrationNumberValidator();
 	
 	@Override
-	public void addBookmark(String username, ICar car) {
+	public void addBookmark(String username, ICar car) throws DuplicateBookmarkException, IllegalUsernameException, IllegalRegistrationNumberException {
 		HashSet<ICar> carHashSet;
 
+		if (username == null || username.equals("")) {
+			throw new IllegalUsernameException();
+		}
+		
+		if (car == null || !registrationNumberValidator.validateRegistrationNumber(car.getRegistrationNumber())) {
+			throw new IllegalRegistrationNumberException();
+		}
+		
 		if (bookmarks.containsKey(username)) {
 			carHashSet = bookmarks.get(username);
 		} else {
@@ -27,11 +41,17 @@ public class BookmarkStorage implements IBookmarkStorage {
 			bookmarks.put(username, carHashSet);
 		}
 		
-		carHashSet.add(car);
+		if (!carHashSet.add(car)) {
+			throw new DuplicateBookmarkException();
+		}
 	}
 
 	@Override
-	public Set<ICar> getBookmarks(String username) {
+	public Set<ICar> getBookmarks(String username) throws IllegalUsernameException {
+		if (username == null || username.equals("")) {
+			throw new IllegalUsernameException();
+		}
+		
 		if (bookmarks.containsKey(username)) {
 			return bookmarks.get(username);
 		} else {
@@ -40,7 +60,15 @@ public class BookmarkStorage implements IBookmarkStorage {
 	}
 
 	@Override
-	public void removeBookmark(String username, String registrationNumber) throws RegistrationNumberNotFoundException {
+	public void removeBookmark(String username, String registrationNumber) throws RegistrationNumberNotFoundException, IllegalRegistrationNumberException, UsernameNotFoundException, IllegalUsernameException {
+		if (username == null || username.equals("")) {
+			throw new IllegalUsernameException();
+		}
+		
+		if (!registrationNumberValidator.validateRegistrationNumber(registrationNumber)) {
+			throw new IllegalRegistrationNumberException();
+		}
+		
 		Car searchCar = new Car();
 		searchCar.setRegistrationNumber(registrationNumber);
 		
@@ -48,6 +76,8 @@ public class BookmarkStorage implements IBookmarkStorage {
 			if (!bookmarks.get(username).remove(searchCar)) {
 				throw new RegistrationNumberNotFoundException();
 			}
+		} else {
+			throw new UsernameNotFoundException();
 		}
 	}
 
@@ -62,4 +92,7 @@ public class BookmarkStorage implements IBookmarkStorage {
 		}
 	}
 
+	public HashMap<String, HashSet<ICar>> getBookmarks() {
+		return bookmarks;
+	}
 }
